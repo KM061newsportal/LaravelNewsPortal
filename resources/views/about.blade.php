f613c1a6060c3db1ad1d13356592a7aa9cb3120a
unknown <bhargavibhintade@gmail.com>
2020-04-03 22:15:37 +0530

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,11 @@
+@extends('layout.main')
+@section('title', 'About')
+@section('menu', 'About')
+@section('content')	
+<div class="card">
+  <div class="card-body">
+		<p>Online news portals are bridge between people and their knowledge of what’s going in outer world. We are leading web development agency, providing an efficient and excellent online readymade news script development services. Our portal comes up with superfast speed of loading, SEO friendly and amazingly responsive Design..</p>
+  </div>
+</div>
+
+@endsection
